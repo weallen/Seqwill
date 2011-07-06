d201945d558679c1c7aa80bc8c3e9410a8a2fd69
Will Allen <we.allen@gmail.com>
2011-07-05 20:23:43 -0700

    Added FileUtil

---
+++ common/FileUtil.cc
@@ -0,0 +1,7 @@
+#include "common/FileUtil.h"
+
+inline void
+GetAllFastaFilesInDirectory(const std::string& directory,
+                            std::vector<std::string>* file_names)
+{
+}
---
+++ common/FileUtil.h
@@ -0,0 +1,18 @@
+#ifndef FILEUTIL_H_
+#define FILEUTIL_H_
+
+#include <iostream>
+#include <vector>
+#include <sys/stat.h>
+
+inline void
+GetAllFastaFilesInDirectory(const std::string& directory,
+                            std::vector<std::string>* file_names);
+
+inline std::string
+GetFilenameWithoutPath(const std::string& filename);
+
+inline std::string
+GetFilenameWithoutExtension(const std::string& filename);
+
+#endif
